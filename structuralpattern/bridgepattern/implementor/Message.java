6517cf763542079ea2c6063ae0ed50781b5f627f
Kavinayasri <146553129+Kavi-654@users.noreply.github.com>
2026-03-25 12:42:32 +0530

    Create Message.java

---
+++ structuralpattern/bridgepattern/implementor/Message.java
@@ -0,0 +1,15 @@
+
+import structuralpattern.bridgepattern.messageformatter.abstraction.MessageSender;
+
+public  abstract class Message {
+
+    protected MessageSender sender;
+    protected String content;
+
+    public Message(MessageSender sender, String content) {
+        this.sender = sender;
+        this.content = content;
+    }
+
+    public abstract void send();
+}
