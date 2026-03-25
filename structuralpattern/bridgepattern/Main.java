77cf6e9f8131cbf9f4a18856c5babd7decc073f6
Kavinayasri <146553129+Kavi-654@users.noreply.github.com>
2026-03-25 12:39:59 +0530

    Create Main.java

---
+++ structuralpattern/bridgepattern/Main.java
@@ -0,0 +1,13 @@
+public class Main
+  {
+public static void main(String[] args) {
+        MessageSender email = new EmailSender();
+        MessageSender sms = new SmsSender();
+
+        Message m1 = new TextMessage(email, "Hello there");
+        Message m2 = new UrgentMessage(sms, "Server is down");
+
+        m1.send();
+        m2.send();
+    }
+}
